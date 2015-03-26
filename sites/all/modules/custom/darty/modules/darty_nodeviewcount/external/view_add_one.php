f0ccd9db5595ff92ac282b003b546af546526b61
binabot <lijian9051@gmail.com>
2015-03-26 14:50:03 +0100

    refaire compteur contenus

--- sites/all/modules/custom/darty/modules/darty_nodeviewcount/external/view_add_one.php
+++ sites/all/modules/custom/darty/modules/darty_nodeviewcount/external/view_add_one.php
@@ -8,22 +8,19 @@
  *   Node id.
  *
  */
+define('DRUPAL_ROOT', getcwd() . '/../../../../../../../../');
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_DATABASE);
 
-$db_config = dirname(__FILE__) . '/../../../../../../../default/settings.local.php';
-$nid       = isset($_POST['nid']) ? intval($_POST['nid']) : '';
+$nid = isset($_POST['nid']) ? htmlspecialchars($_POST['nid'], ENT_QUOTES, 'UTF-8') : 0;
+$nid = intval($nid);
 
-if (file_exists($db_config) && $nid > 0) {
-  include $db_config;
-  // Connect DB
-  $username = $databases['default']['default']['username'];
-  $password = $databases['default']['default']['password'];
-  $hostname = $databases['default']['default']['host'];
-  $database = $databases['default']['default']['database'];
-  $dbhandle = mysqli_connect($hostname, $username, $password, $database) or die("Unable to connect to MySQL");
-  // Query
-  $query = $dbhandle->query("INSERT INTO darty_nodeviewcount (nid, count) VALUES('$nid', 1) ON DUPLICATE KEY UPDATE count=count + 1");
-
-  mysqli_close($dbhandle);
-}
+if ($nid > 0)
+  $query = db_query(
+    "INSERT INTO darty_nodeviewcount (nid, count) VALUES(:nid, 1) ON DUPLICATE KEY UPDATE count=count + 1",
+    array(
+      ':nid' => $nid,
+    )
+  );
 
 ?>
