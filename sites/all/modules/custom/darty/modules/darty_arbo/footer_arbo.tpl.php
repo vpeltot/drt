a67c087854d02f82f75f3a1ebda9dc0b3523f11c
binabot <lijian9051@gmail.com>
2015-05-11 15:48:07 +0200

    nettoyage_du_code

--- sites/all/modules/custom/darty/modules/darty_arbo/footer_arbo.tpl.php
+++ sites/all/modules/custom/darty/modules/darty_arbo/footer_arbo.tpl.php
@@ -4,35 +4,35 @@ $col = 1;
 $max_by_col = 32;
 print '<div class="col1 col">';
 foreach ($arbo_footer as $l1) {
-    if ($cpt > $max_by_col) {
-        $col++;
-        $cpt = 0;
-        print '</div><div class="col' . $col . ' col">';
-    }
-    $cpt+=4;
-    ?>
-    <div class="h2"><a href="<?php print url('taxonomy/term/' . $l1['tid']); ?>#darvousclic=FDV_<?php print ConvertForAnalyticsLink($l1['name']);?>"><?php print $l1['name']; ?></a></div>
-    <?php
-    foreach ($l1['children'] as $l2) {
+  if ($cpt > $max_by_col) {
+    $col++;
+    $cpt = 0;
+    print '</div><div class="col' . $col . ' col">';
+  }
+  $cpt+=4;
+  ?>
+  <div class="h2"><a href="<?php print url('taxonomy/term/' . $l1['tid']); ?>#darvousclic=FDV_<?php print ConvertForAnalyticsLink($l1['name']);?>"><?php print $l1['name']; ?></a></div>
+  <?php
+  foreach ($l1['children'] as $l2) {
 
-        if ($cpt > $max_by_col) {
-            $col++;
-            $cpt = 0;
-            print '</div><div class="col' . $col . ' col">';
-        }
-        $cpt+= 3;
-        ?>
-        <div class="h3"><a href="<?php print url('taxonomy/term/' . $l2['tid']); ?>#darvousclic=FDV_<?php print ConvertForAnalyticsLink($l1['name']);?>_<?php print ConvertForAnalyticsLink($l2['name']);?>"><?php print $l2['name']; ?></a></div>
-        <?php print (!empty($l2['children'])) ? '<ul>' : ''; ?>
-        <?php
-        foreach ($l2['children'] as $l3) {
-            $cpt++;
-            ?>
-            <li><div class="h4"><a href="<?php print url('taxonomy/term/' . $l3['tid']); ?>#darvousclic=FDV_<?php print ConvertForAnalyticsLink($l1['name']);?>_<?php print ConvertForAnalyticsLink($l2['name']);?>_<?php print ConvertForAnalyticsLink($l3['name']);?>"><?php print $l3['name']; ?></a></div></li>
-        <?php } ?>
-        <?php print (!empty($l2['children'])) ? '</ul>' : ''; ?>
+    if ($cpt > $max_by_col) {
+      $col++;
+      $cpt = 0;
+      print '</div><div class="col' . $col . ' col">';
+    }
+    $cpt+= 3;
+    ?>
+    <div class="h3"><a href="<?php print url('taxonomy/term/' . $l2['tid']); ?>#darvousclic=FDV_<?php print ConvertForAnalyticsLink($l1['name']);?>_<?php print ConvertForAnalyticsLink($l2['name']);?>"><?php print $l2['name']; ?></a></div>
+    <?php print (!empty($l2['children'])) ? '<ul>' : ''; ?>
+    <?php
+    foreach ($l2['children'] as $l3) {
+      $cpt++;
+      ?>
+      <li><div class="h4"><a href="<?php print url('taxonomy/term/' . $l3['tid']); ?>#darvousclic=FDV_<?php print ConvertForAnalyticsLink($l1['name']);?>_<?php print ConvertForAnalyticsLink($l2['name']);?>_<?php print ConvertForAnalyticsLink($l3['name']);?>"><?php print $l3['name']; ?></a></div></li>
     <?php } ?>
-    <?php } ?>
+    <?php print (!empty($l2['children'])) ? '</ul>' : ''; ?>
+  <?php } ?>
+  <?php } ?>
 <div class="h2">
 <?php print l('FORUM', 'http://www.36000solutions.com/questions', array('attributes' => array('target' => '_blank'))); ?>
 </div>
